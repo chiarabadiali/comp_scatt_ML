1d9e54e3c312d66f4b9bd8d38504b5c05166d0f0
pabilbado <pablobilbao.25@gmail.com>
2021-03-26 15:08:59 +0100

    New data

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import gdown
 import os
 
-url = 'https://drive.google.com/uc?id=1jaNbWSjjVsm1sFjO5kKe-XnygPyDp-_X'
+url = 'https://drive.google.com/uc?id=1p4N3XeceF38W9e6FEDs36710wd-cn5d7'
 output = 'data.tar.gz'
 gdown.download(url, output, quiet=False) 
 
